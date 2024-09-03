37892ac4772c5cba996af7ce8ee02dcf436f8756
sm9ke <sm9keee@gmail.com>
2024-09-03 17:18:46 +0200

    t

---
+++ Class1.cs
@@ -0,0 +1,15 @@
+﻿using CounterStrikeSharp.API.Core;
+
+namespace cs2plugin;
+
+public class Class1: BasePlugin
+{
+    public override string ModuleName => "Hello World Plugin";
+
+    public override string ModuleVersion => "0.0.1";
+
+    public override void Load(bool hotReload)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
